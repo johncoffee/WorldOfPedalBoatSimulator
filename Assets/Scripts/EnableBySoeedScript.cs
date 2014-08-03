7dbad7f842213c81ec1205c35367b49b0409fd79
Julian <julian@contingent.dk>
2014-08-03 12:21:38 +0200

    skybox and stupid NPCs

---
+++ Assets/Scripts/EnableBySoeedScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableBySoeedScript : MonoBehaviour {
+
+	public ParticleSystem ps;
+	public float psSpeed = 2f;
+
+	Rigidbody _rigidbody;
+
+	void Start() {
+		_rigidbody = this.transform.parent.GetComponent<Rigidbody>();
+		ps = this.GetComponent<ParticleSystem>();
+	}
+
+
+	void Update () {
+
+		if (_rigidbody.velocity.magnitude > psSpeed) {
+			ps.enableEmission = true;
+		}
+		else {
+			ps.enableEmission = false;
+		}
+	}
+}
---
+++ Assets/Scripts/NPCScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCScript : MonoBehaviour {
+
+	public float force = 0.2f;
+	public float maxSpeed = 1f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float x = -force + Random.value * force * 2f * Time.deltaTime;
+		float z = -force + Random.value * force * 2f * Time.deltaTime;
+
+		this.rigidbody.velocity += new Vector3(x, 0, z);	
+
+		if (this.rigidbody.velocity.magnitude > maxSpeed) {
+			Vector3 v = rigidbody.velocity;
+			v = v * 0.9f;
+			rigidbody.velocity = v;
+		}
+
+		if (Random.value > 0.97f) {
+			rigidbody.AddRelativeTorque( new Vector3( 0, Random.value * 360f, 0));
+		}
+	}
+}
