202bca020d96258407bb117e49299bfa484a5444
Julian <julian@contingent.dk>
2014-08-03 10:12:47 +0200

    race leve

---
+++ Assets/GoalScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalScript : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Return)) {
+			Application.LoadLevel(0);
+		}
+	}
+
+	void OnTriggerEnter() {
+		Debug.Log("Player something has won");
+	}
+}
